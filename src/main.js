a1c4f7d60c6b1efb5189d72ea249cfb50a3fec27
Tsong Lew <tsonglew@gmail.com>
2025-02-23 23:41:52 +0800

    feat: pdf preview

--- src/main.js
+++ src/main.js
@@ -5,12 +5,13 @@ let greetMsgEl;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.innerHTML });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
+
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
